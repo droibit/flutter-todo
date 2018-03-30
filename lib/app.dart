d6429bd5a2558da446d590b705951e3eb3092b29
Shinya Kumagai <roomful.rooms@gmail.com>
2018-03-30 14:10:20 +0900

    Update app theme

--- lib/app.dart
+++ lib/app.dart
@@ -14,21 +14,9 @@ class TodoApp extends StatelessWidget {
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
       ],
-      supportedLocales: [
-        const Locale('en', ''),
-        const Locale('ja', '')
-      ],
+      supportedLocales: [const Locale('en', ''), const Locale('ja', '')],
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: Colors.blueGrey, accentColor: Colors.redAccent),
       home: new MyHomePage(),
     );
   }
