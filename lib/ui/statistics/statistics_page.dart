04f8bf320409370d9e1df6c2d405e5e8b7dab224
Shinya Kumagai <roomful.rooms@gmail.com>
2018-04-01 01:47:42 +0900

    Add StatisticsPage

---
+++ lib/ui/statistics/statistics_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../../i10n/app_localizations.dart';
+import '../app_drawer.dart';
+
+class StatisticsPage extends StatefulWidget {
+  StatisticsPage({Key key}) : super(key: key);
+
+  @override
+  State createState() => new StatisticsPageState();
+}
+
+class StatisticsPageState extends State<StatisticsPage> {
+  @override
+  Widget build(BuildContext context) {
+    final title = AppLocalizations.of(context).statistics;
+    return new Scaffold(
+      drawer: new AppDrawer(selectedNavigation: NavigationId.statistics),
+      appBar: new AppBar(
+        title: new Text(title),
+      ),
+      body: new Center(
+        child: new Text(title),
+      ),
+    );
+  }
+}
