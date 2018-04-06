a1e4302f3fae4e2a11a96246ead6d69e29a0c9ac
Shinya Kumagai <roomful.rooms@gmail.com>
2018-04-06 23:17:59 +0900

    Modify to use model.PackageInfo

--- lib/middleware/package_info_middleware.dart
+++ lib/middleware/package_info_middleware.dart
@@ -4,15 +4,21 @@ import '../action/package_info_action.dart';
 import '../data/provider/package_provider.dart';
 import '../model/model.dart';
 
-List<Middleware<AppState>> createPackageInfoMiddleware(PackageProvider packageProvider) {
+List<Middleware<AppState>> createPackageInfoMiddleware(
+    PackageProvider packageProvider) {
   return <Middleware<AppState>>[
-    _getPackageInfoMiddleware(packageProvider),
+    new TypedMiddleware<AppState, GetPackageInfoAction>(
+      _getPackageInfoMiddleware(packageProvider),
+    ),
   ];
 }
 
-Middleware<AppState> _getPackageInfoMiddleware(PackageProvider packageProvider) {
+Middleware<AppState> _getPackageInfoMiddleware(
+    PackageProvider packageProvider) {
   return (Store<AppState> store, dynamic action, NextDispatcher next) async {
-    final packageInfo = await packageProvider.get();
-    next(new OnGetPackageInfoAction(packageInfo));
+    final src = await packageProvider.get();
+    next(new OnGetPackageInfoAction(
+      new PackageInfo(version: src.version),
+    ));
   };
 }
