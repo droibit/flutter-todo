237eef8f705537866491a7c5bb1cdf416d39a477
Shinya Kumagai <roomful.rooms@gmail.com>
2018-04-07 17:34:05 +0900

    Add empty _EmptyView and _TaskListView

--- lib/ui/tasks/tasks_page.dart
+++ lib/ui/tasks/tasks_page.dart
@@ -1,26 +1,64 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 
+import '../../action/task_action.dart';
 import '../../i10n/app_localizations.dart';
+import '../../model/model.dart';
 import '../app_drawer.dart';
 
-class TasksPage extends StatefulWidget {
+class TasksPage extends StatelessWidget {
   TasksPage({Key key}) : super(key: key);
 
   @override
-  State createState() => TasksPageState();
-}
-
-class TasksPageState extends State<TasksPage> {
-  @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      drawer: new AppDrawer(selectedNavigation: NavigationId.tasks),
-      appBar: new AppBar(
-        title: new Text(AppLocalizations.of(context).title),
+      drawer: new AppDrawer(
+          selectedNavigation: NavigationId.tasks,
       ),
+      appBar: new AppBar(title: new Text(AppLocalizations.of(context).title)),
       body: new Center(
-        child: const Text('Hello, world'),
+        child: new StoreConnector<AppState, List<Task>>(
+          distinct: true,
+          onInit: (store) => store.dispatch(new GetTasksAction()),
+          converter: (store) => store.state.tasks,
+          builder: (context, tasks) {
+            if (tasks.isEmpty) {
+              return new _EmptyView();
+            } else {
+              return new _TaskListView(tasks);
+            }
+          },
+        ),
       ),
+    );
+  }
+}
+
+class _EmptyView extends StatelessWidget {
+  _EmptyView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+      child: const Text('No TO-DO.'),
+    );
+  }
+}
+
+class _TaskListView extends StatelessWidget {
+  final List<Task> _tasks;
+
+  _TaskListView(
+    this._tasks, {
+    Key key,
+  })  : assert(_tasks?.isNotEmpty),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+      child: const Text('TO-DO.'),
     );
   }
 }
